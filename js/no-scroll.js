b7726845683711d2b94aedc8eed09c51f8c4631a
Percival Aceron <perci.aceron@gmail.com>
2023-09-21 15:34:35 +0800

    no-scroll.js fix

--- js/no-scroll.js
+++ js/no-scroll.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $('.modal-open, .js-open-menu').on('click', function () {
     $('body').css('overflow', 'hidden');
-    $('.modal, .js-menu-container').fadeIn();
+    $('.modal, .js-menu-container');
   });
 
   $('.modal-close, .js-close-menu').on('click', function () {
     $('body').css('overflow', 'auto');
-    $('.modal, .js-menu-container').fadeOut();
+    $('.modal, .js-menu-container');
   });
 });
